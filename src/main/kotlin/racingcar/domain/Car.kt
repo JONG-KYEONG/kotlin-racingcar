df19fa1f70029a17690fb47c78e010c9f0a01479
JongKyeong Kim <oo7bangjk@naver.com>
2024-08-12 16:53:05 +0900

    refactor: add car_move_condition

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -6,7 +6,7 @@ class Car(val name : String){
     var position : Int = 0
         private set
     init {
-        require(name.length <= 5) { "자동차 이름은 5글자를 초과할 수 없습니다." }
+        require(name.length <= CAR_NAME_LENGTH) { "자동차 이름은 5글자를 초과할 수 없습니다." }
         require(!name.contains(" ")) { "자동차 이름은 공백을 포함 할 수 없습니다." }
     }
     fun move(num : Int){
@@ -14,5 +14,6 @@ class Car(val name : String){
     }
     companion object {
         private const val CAR_MOVE_CONDITION = 4
+        private const val CAR_NAME_LENGTH = 5
     }
 }
