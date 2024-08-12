3c407676a7c1476adf24f261559930cd2ddb4644
JongKyeong Kim <oo7bangjk@naver.com>
2024-08-12 14:43:02 +0900

    refactor: refact car.position setter

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -2,13 +2,15 @@ package racingcar.domain
 
 import kotlin.random.Random
 
-class Car(val name : String, var position : Int = 0){
+class Car(val name : String){
+    var position : Int = 0
+        private set
     init {
         require(name.length <= 5) { "자동차 이름은 5글자를 초과할 수 없습니다." }
         require(!name.contains(" ")) { "자동차 이름은 공백을 포함 할 수 없습니다." }
     }
-    fun move(randomNum : Int){
-        if(randomNum >= CAR_MOVE_CONDITION) position++
+    fun move(num : Int){
+        if(num >= CAR_MOVE_CONDITION) position++
     }
     companion object {
         private const val CAR_MOVE_CONDITION = 4
