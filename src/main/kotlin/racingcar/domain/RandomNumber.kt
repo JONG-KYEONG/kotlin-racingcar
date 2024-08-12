86c219fa776033d4560ef9375a7e9f501fb4f5a8
JongKyeong Kim <oo7bangjk@naver.com>
2024-08-12 14:52:12 +0900

    refactor: refact getRandomNumber method

--- src/main/kotlin/racingcar/domain/RandomNumber.kt
+++ src/main/kotlin/racingcar/domain/RandomNumber.kt
@@ -3,9 +3,5 @@ package racingcar.domain
 class RandomNumber {
     private val MIN_NUMBER_RANGE = 0
     private val MAX_NUMBER_RANGE = 9
-    fun getRandomNumber(): Int{
-        val numberRange = (MIN_NUMBER_RANGE..MAX_NUMBER_RANGE)
-        val num = numberRange.random()
-        return num
-    }
+    fun getRandomNumber(): Int = (MIN_NUMBER_RANGE..MAX_NUMBER_RANGE).random()
 }
